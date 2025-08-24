56bcdc047f8af80274a3b5fe65b7bdaf9a86b14a
Anderson de Oliveira <anderson.diolv@gmail.com>
2025-08-24 16:53:25 -0300

    Update

--- src/config.py
+++ src/config.py
@@ -6,7 +6,3 @@ KAFKA_TOPIC = os.getenv("KAFKA_TOPIC", "pedidos_ecommerce")
 
 # PostgreSQL
 POSTGRES_CONN = os.getenv("POSTGRES_CONN", "postgresql://app:app@localhost:5432/ecommerce")
-
-# API (exemplo)
-ECOM_API_BASE = os.getenv("ECOM_API_BASE", "https://api.minhaloja.com")
-ECOM_API_KEY = os.getenv("ECOM_API_KEY", "SUA_CHAVE_AQUI")
